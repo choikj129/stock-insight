258fe8c3917958c9be791d4a72975fd7006b0266
choikj129 <87593382+choikj129@users.noreply.github.com>
2026-09-24 19:25:41 +0900

    feat: initialize project

---
+++ src/main/java/org/stockinsight/stockinsight/StockInsightApplication.java
@@ -0,0 +1,13 @@
+package org.stockinsight.stockinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockInsightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/stockinsight/stockinsight/StockInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stockinsight.stockinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
